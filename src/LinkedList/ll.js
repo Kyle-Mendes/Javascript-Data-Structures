cafcb5276714be9e51f4b0aac3f8eab1ef152bc4
Kyle Mendes <kmendes@fuzzproductions.com>
2016-07-05 17:05:20 -0400

    Initial commit

---
+++ src/LinkedList/ll.js
@@ -0,0 +1,229 @@
+class Node {
+	constructor(data, next) {
+		this.data = data;
+		this.next = next;
+	}
+}
+
+export default class LinkedList {
+	constructor(head) {
+		this.head = head || null;
+	}
+
+	_tail() {
+		let tmp = this.head;
+
+		if (tmp) {
+
+			while(tmp.next) {
+				tmp = tmp.next;
+			}
+
+			return tmp;
+		} else {
+			return null;
+		}
+	}
+
+	_throwEmpty() {
+		let err = new ReferenceError('List is empty.');
+		throw err;
+	}
+
+	/**
+	 * A function to see if the list is empty or not;
+	 *
+	 * @return {boolean} Whether the list is empty or not.
+	 */
+	isEmpty() {
+		return this.head === null;
+	}
+
+	// Maybe return the `node` so that the dev has access to `data` and `next`
+	// should `data` be changed to `value`?
+	includes(val) {
+		let current = this.head;
+
+		while (current) {
+			if (current.data === val) {
+				return current;
+			}
+
+			current = current.next;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Empties the list by setting `this.head` to a null value.
+	 *
+	 * @return {null}
+	 */
+	clear() {
+		this.head = null;
+	}
+
+	/**
+	 * Returns the value of the first node WITHOUT deleting it from the list.
+	 *
+	 * @return {*} The value of the first node in the list.
+	 */
+	getHead() {
+		if (! this.isEmpty()) {
+			return this.head.data;
+		} else {
+			this._throwEmpty();
+		}
+	}
+
+	/**
+	 * Returns the value of the last node WITHOUT deleting it from the list.
+	 *
+	 * @return {*} The value of the last node in the list.
+	 */
+	getTail() {
+		let tail = this._tail();
+		if (tail) {
+			return this._tail().data;
+		} else {
+			this._throwEmpty();
+		}
+	}
+
+	/**
+	 * Returns the value of the first node in the list and removes it from the list.
+	 * If the Node has a `next` value, sets that Node to `this.head`.
+	 * Otherwise sets head to `null`.
+	 *
+	 * @return {*} The value of the first node in the list.
+	 */
+	shift() {
+		if (this.head) {
+			let tmp = this.head;
+			this.head = this.head.next;
+
+			return tmp.data;
+		} else {
+			this._throwEmpty();
+		}
+	}
+
+	/**
+	 * Returns the value of the last node in the list and removes it from the list.
+	 * Also, takes the next-to-last node, and sets it's `next` pointer to `null`.
+	 *
+	 * @return {*} The value of the last node in the list.
+	 */
+	pop() {
+		let tmp = this.head;
+
+		if (tmp) {
+
+			while(tmp.next.next) {
+				tmp = tmp.next;
+			}
+
+			let tail = tmp.next;
+			tmp.next = null;
+
+			return tail.data;
+		} else {
+			this._throwEmpty();
+		}
+	}
+
+	/**
+	 * Adds a new Node to the front of the list.
+	 *
+	 * @return {null}
+	 */
+	prepend(data) {
+		let tmp = this.head || null;
+
+		this.head = new Node(data, tmp);
+	}
+
+	/**
+	 * Adds a new Node to the end of the list.
+	 *
+	 * @return {null}
+	 */
+	append(data) {
+		if (this.head === null) {
+			this.prepend(data);
+		} else {
+			let tmp = this._tail();
+
+			tmp.next = new Node(data, null);
+		}
+	}
+
+	/**
+	 * Loops through the list and creates an Array from all of the Nodes in the list.
+	 *
+	 * @return {Array<*>}
+	 */
+	toArray() {
+		let array = [];
+
+		if (this.isEmpty()) {
+			this._throwEmpty();
+		}
+
+		let tmp = this.head;
+		while (tmp.next) {
+			array.push(tmp.data);
+			tmp = tmp.next;
+		}
+
+		// Make sure we push the last value!
+		array.push(tmp.data)
+
+		return array;
+	}
+
+	/**
+	 * Clears the list and fills it with new Nodes from the values of an array.
+	 *
+	 * @return {null}
+	 */
+	fromArray(array) {
+		this.head = null;
+
+		for (let i = array.length - 1; i > -1; i--) {
+			let tmp = this.head;
+
+			this.head = new Node(array[i], tmp);
+		}
+	}
+
+	/**
+	 * Appends an array to the end of the list.
+	 *
+	 * @return {null}
+	 */
+	addArray(array) {
+		let tail = this._tail();
+
+		array.forEach((val) => {
+			tail.next = new Node(val, null);
+			tail = tail.next;
+		});
+	}
+
+	reverse() {
+		let current = this.head,
+			previous,
+			next;
+
+			while (current) {
+				next = current.next;
+				current.next = previous;
+				previous = current;
+				current = next;
+			}
+
+			this.head = previous;
+	}
+}
---
+++ test/ll_test.js
@@ -0,0 +1,125 @@
+// mocha --compilers js:babel-core/register LinkedList/test.js
+import LinkedList from "../src/LinkedList/ll";
+
+var expect = require('chai').expect;
+
+describe('LinkedLst', () => {
+	var ll;
+
+	it('Can instantiate', () => {
+		expect(new LinkedList).to.be.ok;
+	});
+
+	describe('adding values', () => {
+		beforeEach(() => {
+			ll = new LinkedList();
+		});
+
+		it ('Can add a value to the head of the list', () => {
+			ll.prepend('a');
+			expect(ll.head.data).to.equal('a');
+
+			ll.prepend('b');
+			expect(ll.head.data).to.equal('b');
+			expect(ll.head.next.data).to.equal('a');
+		});
+
+		it ('Can add a value to the tail of the list', () => {
+			ll.append('a');
+			expect(ll.head.data).to.equal('a');
+
+			ll.append('b');
+			expect(ll.head.data).to.equal('a');
+			expect(ll.head.next.data).to.equal('b');
+		});
+
+		it('Can create a Linked List from an array', () => {
+			ll.fromArray([1, 2, 3]);
+
+			expect(ll.head.data).to.equal(1);
+			expect(ll.head.next.data).to.equal(2);
+			expect(ll.head.next.next.data).to.equal(3);
+		});
+
+		it('Can add values to a Linked List from an array', () => {
+			ll.fromArray([1, 2, 3]);
+			ll.addArray(['a', 'b', 'c']);
+
+			expect(ll.head.data).to.equal(1);
+			expect(ll.head.next.data).to.equal(2);
+			expect(ll.head.next.next.data).to.equal(3);
+
+			let old = ll.head.next.next;
+
+			expect(old.next.data).to.equal('a');
+			expect(old.next.next.data).to.equal('b');
+			expect(old.next.next.next.data).to.equal('c');
+		});
+	});
+
+	beforeEach(() => {
+		ll = new LinkedList();
+		ll.fromArray([1, 2, 3, 'a', 'b', 'c']);
+	});
+
+	describe('querying', () => {
+		it('Can return the head\'s value', () => {
+			expect(ll.getHead()).to.equal(1);
+		});
+
+		it('Can return the tail\'s value', () => {
+			expect(ll.getTail()).to.equal('c');
+		});
+
+		it('Can find a value in a list', () => {
+			expect(ll.includes('not there')).to.be.false;
+			expect(ll.includes('b')).to.be.ok;
+		});
+
+		it('Can test for empty lists', () => {
+			let l = new LinkedList();
+
+			expect(ll.isEmpty()).to.be.false;
+			expect(l.isEmpty()).to.be.true;
+		});
+
+		it('Can export a list as an array', () => {
+			expect(ll.toArray()).to.eql([1, 2, 3, 'a', 'b', 'c']);
+		});
+
+		it('Can clear a list of all entries', () => {
+			expect(ll.getHead()).to.exist;
+			expect(ll.head).to.exist;
+
+			ll.clear();
+			expect(ll.head).to.not.exist;
+			expect(ll.getHead.bind(ll)).to.throw(ReferenceError);
+			expect(ll.getTail.bind(ll)).to.throw(ReferenceError);
+			expect(ll.shift.bind(ll)).to.throw(ReferenceError);
+			expect(ll.pop.bind(ll)).to.throw(ReferenceError);
+			expect(ll.toArray.bind(ll)).to.throw(ReferenceError);
+		});
+	});
+
+	describe('mutating', () => {
+		it('Can remove the head\'s value', () => {
+			expect(ll.shift()).to.equal(1);
+			expect(ll.getHead()).to.equal(2);
+		});
+
+		it('Can remove the tail\'s value', () => {
+			expect(ll.pop()).to.equal('c');
+			expect(ll.getTail()).to.equal('b');
+		});
+
+		it('Can reverse a list', () => {
+			ll.reverse();
+
+			expect(ll.getHead()).to.equal('c');
+			expect(ll.getTail()).to.equal(1);
+		});
+
+		it('Can return the head\'s value', () => {
+		});
+	});
+});
